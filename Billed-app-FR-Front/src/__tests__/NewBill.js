5585b5b26a62b007b9a81f159af49146ff1c85d1
Cindy Lionis <cindy.lionis@gmail.com>
2026-05-13 17:06:01 +0200

    fix: ajout de tests pour l'affichage du formulaire dans NewBill

--- Billed-app-FR-Front/src/__tests__/NewBill.js
+++ Billed-app-FR-Front/src/__tests__/NewBill.js
@@ -2,17 +2,52 @@
  * @jest-environment jsdom
  */
 
-import { screen } from "@testing-library/dom"
-import NewBillUI from "../views/NewBillUI.js"
-import NewBill from "../containers/NewBill.js"
+import { screen } from "@testing-library/dom";
+import { localStorageMock } from "../__mocks__/localStorage.js";
+import mockStore from "../__mocks__/store.js";
+import { ROUTES } from "../constants/routes";
+import NewBill from "../containers/NewBill.js";
+import NewBillUI from "../views/NewBillUI.js";
 
+jest.mock("../app/store", () => mockStore)
 
 describe("Given I am connected as an employee", () => {
   describe("When I am on NewBill Page", () => {
-    test("Then ...", () => {
+    test("Then the form should be displayed with all input fields", () => {
+      Object.defineProperty(window, 'localStorage', { value: localStorageMock })
+      window.localStorage.setItem('user', JSON.stringify({
+        type: 'Employee',
+        email: 'employee@test.com'
+      }))
+      
+      const onNavigate = (pathname) => {
+        document.body.innerHTML = ROUTES({ pathname })
+      }
+
       const html = NewBillUI()
       document.body.innerHTML = html
-      //to-do write assertion
+
+      const newBillContainer = new NewBill({
+        document,
+        onNavigate,
+        store: mockStore,
+        localStorage: window.localStorage
+      })
+
+      const form = screen.getByTestId('form-new-bill')
+      expect(form).toBeTruthy()
+
+      expect(screen.getByTestId('expense-type')).toBeTruthy()
+      expect(screen.getByTestId('expense-name')).toBeTruthy()
+      expect(screen.getByTestId('datepicker')).toBeTruthy()
+      expect(screen.getByTestId('amount')).toBeTruthy()
+      expect(screen.getByTestId('vat')).toBeTruthy()
+      expect(screen.getByTestId('pct')).toBeTruthy()
+      expect(screen.getByTestId('commentary')).toBeTruthy()
+      expect(screen.getByTestId('file')).toBeTruthy()
+
+      const submitButton = screen.getByRole('button', { name: /Envoyer/i })
+      expect(submitButton).toBeTruthy()
     })
   })
 })
